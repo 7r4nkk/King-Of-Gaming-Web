0bec9083ceaf22a9d31520a0507bdcfebfd511ac
frankfobello <fobellofrank@gmail.com>
2022-05-17 22:48:10 -0300

    Entrega de pre-entrega

--- src/components/ItemListContainer1/Products.js
+++ src/components/ItemListContainer1/Products.js
@@ -126,8 +126,8 @@ const product=[{
     subname: 'GPU NVIDIA RTX',
     price: 99.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652591414/compragamer_Imganen_general_27836_Placa_de_Video_ASUS_GeForce_RTX_3090_24GB_GDDR6X_TUF_GAMING_OC_c5a7da31-grn-removebg-preview_w2ukwz.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'Las GeForce RTX™ 3090 Ti y 3090 son implacables GPUs (BFGPU) con rendimiento de clase TITAN. Impulsadas con tecnología Ampere, la arquitectura RTX de segunda generación de NVIDIA, duplican el ray tracing y el rendimiento de la IA con núcleos de trazado de rayos mejorados, núcleos Tensor y nuevos multiprocesadores de transmisión. Además, cuentan con 24 GB de asombrosa memoria G6X para ofrecer la experiencia de juego definitiva a jugadores y creadores.',
+    categoria: 'placasdevideo'
 },
 {
     id:'6',
@@ -135,8 +135,8 @@ const product=[{
     subname: 'GPU NVIDIA RTX',
     price: 89.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652591414/compragamer_Imganen_general_27930_Placa_de_Video_MSI_GeForce_RTX_3080_Ti_12GB_GDDR6X_VENTUS_3X_OC_LHR_8a87fc67-grn-removebg-preview_ebkmfm.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'Las tarjetas gráficas GeForce RTX™ 3080 Ti y RTX 3080 ofrecen el alto rendimiento que buscan los gamers. Basado en Ampere, la segunda generación de la arquitectura RTX de NVIDIA, cuentan con Núcleos RT mejorados y Núcleos Tensor, nuevos multiprocesadores de transmisión y memorias GDDR6X súper rápidas, para una experiencia de juego simplemente increíble.',
+    categoria: 'placasdevideo'
 },
 {
     id:'7',
@@ -144,8 +144,8 @@ const product=[{
     subname: 'GPU NVIDIA RTX',
     price: 79.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652591414/compragamer_Imganen_general_29023_Placa_de_Video_MSI_GeForce_RTX_3070_Ti_8GB_GDDR6X_GAMING_X_TRIO_b4cd3e9a-grn-removebg-preview_oryomu.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'Las tarjetas gráficas de la familia GeForce RTX™ 3070Ti cuentan con tecnología Ampere, la arquitectura RTX de segunda generación de NVIDIA Se diseñaron con los Núcleos RT y Núcleos Tensor mejorados, nuevos multiprocesadores de transmisión y una memoria de alta velocidad,  ofreciendo la potencia que necesitas para los juegos más exigentes.',
+    categoria: 'placasdevideo'
 },
 {
     id:'8',
@@ -153,8 +153,8 @@ const product=[{
     subname: 'GPU NVIDIA RTX',
     price: 69.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652591414/compragamer_Imganen_general_32128_Placa_de_Video_ASUS_GeForce_RTX_3060_12GB_GDDR6_TUF_GAMING_OC_V2__LHR_c1152d97-grn-removebg-preview_zuo3sg.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'La GeForce RTX™ 3060 Ti y la RTX 3060 te permiten disfrutar de los juegos más recientes con la potencia de Ampere, la segunda generación de la arquitectura RTX de NVIDIA. Logra un rendimiento increíble con los Núcleos Ray Tracing y los Núcleos Tensor mejorados, los nuevos multiprocesadores de streaming y la memoria G6 de alta velocidad.',
+    categoria: 'placasdevideo'
 },
 {
     id:'9',
@@ -162,8 +162,8 @@ const product=[{
     subname: 'Motherboard ASUS AMD AM4',
     price: 99.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652592623/compragamer_Imganen_general_31131_Mother_ASUS_ROG_X570_CROSSHAIR_VIII_EXTREME_7de54ab4-grn-removebg-preview_ulbmfu.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'Tarjeta madre para juegos AMD X570 EATX con 18 + 2 fases de poder, cinco puertos M.2, conector del panel frontal USB 3.2 Gen 2x2 con soporte para PD 3.0 de 60W, conector del panel frontal USB 3.2 Gen 2, Thunderbolt™ 4 doble, Marvell® AQtion 10 Gb Ethernet, Ethernet Intel® de 2.5 Gb, PCIe® 4.0, WiFi 6E integrado e iluminación Aura Sync RGB.',
+    categoria: 'motherboards'
 },
 {
     id:'10',
@@ -171,8 +171,8 @@ const product=[{
     subname: 'Motherboard ASUS AMD AM4',
     price: 89.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652592623/compragamer_Imganen_general_19859_Mother_ASUS_ROG_STRIX_B550-E_Gaming_AM4_335fec01-grn-removebg-preview_ffgm7h.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'Las tarjetas madre de la serie ROG Strix B550 Gaming ofrecen buena parte de las características que puedes encontrar en la serie de gama alta ROG Strix X570 Gaming, como por ejemplo PCIe® 4.0. Con un suministro de energía potente y una refrigeración eficaz, la serie ROG Strix B550 Gaming está optimizada para las CPU AMD Ryzen™ de 3.ª Gen. Con una estética futurista y el intuitivo software ROG, la ROG Strix B550-E Gaming es la plataforma ideal para montar el equipo de tus sueños.',
+    categoria: 'motherboards'
 },
 {
     id:'11',
@@ -180,8 +180,8 @@ const product=[{
     subname: 'Motherboard ASUS AMD AM4',
     price: 79.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652592624/compragamer_Imganen_general_22370_Mother_ASUS_ROG_STRIX_B450-F_GAMING_II_AM4_c17380b9-grn-removebg-preview_rkzeje.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'ROG Strix B450-F Gaming II ofrece todos los elementos esenciales necesarios para una construcción bien equilibrada. Diseñado para manejar los últimos procesadores AMD Ryzen ™ de tercera generación, ROG Strix B450-F Gaming II presenta una entrega de energía mejorada, un diseño térmico optimizado y una memoria DDR4 más estable. BIOS FlashBack™ facilita la actualización para darle una ventaja a cualquier construcción de ensueño, y el intuitivo software ROG garantiza una experiencia de juego superior.',
+    categoria: 'motherboards'
 },
 {
     id:'12',
@@ -189,8 +189,8 @@ const product=[{
     subname: 'Motherboard ASUS AMD AM4',
     price: 69.99,
     image:'https://res.cloudinary.com/dkgjwdz8j/image/upload/v1652592624/compragamer_Imganen_general_32227_Mother_Asus_Prime_A320I-K_DDR4_ITX_AM4_138de92a-grn-removebg-preview_qhxyia.png',
-    details:'Clave en el rendimiento de tu computador de escritorio, ya no tienes que pensar en cómo distribuir el tiempo y acciones porque ahora las tareas en simultáneo son posibles. AMD cuenta con un catálogo de productos que se adaptan a los requerimientos de todo tipo de usuarios: juegos en línea, edición a gran escala, contenido en múltiples plataformas y más.',
-    categoria: 'procesadores'
+    details:'ASUS Prime es la próxima evolución de la placa base ASUS, nacida de un pedigrí que se remonta a 1989. Nuestro equipo de ingenieros de clase mundial está apasionado por capacitar a todos para que disfruten de los beneficios de la personalización y el ajuste, y la fuerza impulsora detrás del desarrollo de Prime es hacer que los controles avanzados para entusiastas sean fácilmente accesibles, maximizando el rendimiento, la estabilidad y la compatibilidad para las mejores experiencias de PC de bricolaje. En pocas palabras: es hora de Prime.',
+    categoria: 'motherboards'
 }
 ]
 export const traerProductos = (categoriaId) => {
