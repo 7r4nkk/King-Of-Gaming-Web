0b26936eabefc12d9065d988f33d5469d0513bd3
frankfobello <fobellofrank@gmail.com>
2022-06-06 20:59:33 -0300

    Arreglo en vista mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 return (
     <div className='bodyhome'>
-    <div className='main-image'>
-        <div className='containerhome'>
-            <h1 data-aos="fade-up" data-aos-duration="2500"><span className='king'>KING OF <span className='gamingg'>GAMING</span></span></h1>
-            <Link to='/categoria/procesadores'style={{ textDecoration: 'none' }}>    
-                <button className="btt draw-border" data-aos="fade-down" data-aos-duration="3000">CPU</button>
-            </Link>
-            <Link to='/categoria/placasdevideo' style={{ textDecoration: 'none' }}>
-                <button className="btt draw-border" data-aos="fade-down" data-aos-duration="3000">GPU</button>
-            </Link>
-            <Link to='/categoria/motherboards' style={{ textDecoration: 'none' }}>
-                <button className="btt draw-border" data-aos="fade-down" data-aos-duration="3000">MOBO</button>
-            </Link>
+        <div className='main-image'>
+            <div className='containerhome'>
+                <h1><span className='king'>KING OF <span className='gamingg'>GAMING</span></span></h1>
+                <Link to='/categoria/procesadores'style={{ textDecoration: 'none' }}>    
+                    <button className="btt draw-border">CPU</button>
+                </Link>
+                <Link to='/categoria/placasdevideo' style={{ textDecoration: 'none' }}>
+                    <button className="btt draw-border">GPU</button>
+                </Link>
+                <Link to='/categoria/motherboards' style={{ textDecoration: 'none' }}>
+                    <button className="btt draw-border">MOBO</button>
+                </Link>
+            </div>
         </div>
-    </div>
     </div>
 )
 }
